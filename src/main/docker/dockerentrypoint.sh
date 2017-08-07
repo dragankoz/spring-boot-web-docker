0ccbf248b2af59bb81f3d70e5136b8c129496d58
DraganKocovski <dragan.k.oz@gmail.com>
2017-08-07 16:02:29 +1000

    Setup docker to create and use the user 'app' to start the application

--- src/main/docker/dockerentrypoint.sh
+++ src/main/docker/dockerentrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
-
-adduser app -D
-chown app:app /usr/local/*
 JAVA_OPTS=${JAVA_OPTS:="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XshowSettings:vm"}
-exec su-exec app java ${JAVA_OPTS} -jar /usr/local/ROOT.war --server.port 8080
+exec su-exec app java ${JAVA_OPTS} -jar /opt/app/ROOT.war --server.port 8080
