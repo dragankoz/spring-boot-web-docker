21b179d5802a0bf395e67bb50c6aa15425afb814
Dragan Kocovski <dragan.k.oz@gmail.com>
2017-09-09 01:57:31 +1000

    Make dockerfile simpler

--- src/main/docker/dockerentrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-JAVA_OPTS=${JAVA_OPTS:="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XshowSettings:vm"}
-exec su-exec app java ${JAVA_OPTS} -jar /opt/app/ROOT.war --server.port 8080
